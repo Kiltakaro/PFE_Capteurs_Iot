ce05c8cdb9c24f791845630907d522a151714524
Kiltakaro <guboot08@gmail.com>
2025-02-17 18:19:57 +0000

    split users routes

--- backend/main.py
+++ backend/main.py
@@ -1,33 +1,30 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Blueprint
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import check_password_hash, generate_password_hash
-from threading import Thread
-import os, uuid, time
-
-import datetime
-from flask import Blueprint
 from werkzeug.utils import secure_filename
-import csv
 
-import paho.mqtt.client as mqtt
-import json
-import random
 from apscheduler.schedulers.background import BackgroundScheduler
-import logging
+from threading import Thread 
+ 
+import paho.mqtt.client as mqtt
+
+import os, uuid, time, sys, logging, random, json, csv, datetime
+
+from scipy.interpolate import interp1d 
 import pandas as pd
-import sys
-from scipy.interpolate import interp1d
 import numpy as np
+
+
+from models import db, User, SensorData, SensorHistory
+
+from users import users_bp
 
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
-
-
-from models import db, User, SensorData, SensorHistory
 
 time.sleep(5)
 
@@ -105,143 +102,6 @@ jwt = JWTManager(app)
 @app.route('/hello_world')
 def hello_world():
     return "Hello world !"
-
-
-######################  ACCOUNTS #######################
-#####################  CRUD  USER  #####################
-@app.route('/api/users/create', methods=['POST'])
-def create_user():
-    """
-    Route pour créer un User dans la database
-    """
-    data = request.json
-    username = data.get('username')
-    password = data.get('password')
-    is_admin = data.get('is_admin', False)
-
-    if not username or not password:
-        return jsonify({"error": "Nom d'utilisateur et Mot de passe requis"}), 400
-
-    # Vérification si le nom d'utilisateur existe déjà
-    if User.query.filter_by(username=username).first():
-        return jsonify({"error": "Ustateur déjà existant"}), 400
-
-    # Création de l'utilisateur
-    new_user = User(username=username, is_admin=is_admin)
-    new_user.set_password(password)
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({"message": "Utilisateur créé"}), 201
-
-
-# ROute a proteger je pense
-@app.route('/api/users', methods=['GET'])
-def read_users():
-    """
-    Route pour récupérer tous users
-    """
-    users = User.query.all()
-    return jsonify([{
-        "id": user.id,
-        "username": user.username,
-        "is_admin": user.is_admin
-    } for user in users]), 200
-
-
-# CELLE DU DESSUS CEST POUR TOUS LES USERS
-# CELLE DU DESSOUS C'EST POUR SEULEMENT CELUI QUI EST CONNECTE
-@app.route('/api/user', methods=['GET'])
-@jwt_required()
-def read_user():
-    """
-    Route pour récupérer l'utilisateur actuel 
-    """
-    current_user = get_jwt_identity()
-    user = User.query.filter_by(username=current_user).first()
-    if not user:
-        return jsonify({"msg": "Utilisateur non existant"}), 404
-    return jsonify({
-        "id": user.id,
-        "username": user.username,
-        "is_admin": user.is_admin
-    }), 200
-
-# Il faudra peut etre rajouter un cas ou c'est l'admin qui veut modifier
-# Route pour mettre à jour un utilisateur
-@app.route('/api/user', methods=['PUT'])
-@jwt_required()
-def update_user():
-    """
-    Route pour que l'utilisateur modifie ses données
-
-    Arguments attendus
-    username : nouveau nom de l'utilisateur
-    password : nouveau mot de passe de l'utilisateur
-    """
-    current_user = get_jwt_identity()
-    user = User.query.filter_by(username=current_user).first()
-    if not user:
-        return jsonify({"msg": "Utilisateur non existant"}), 404
-
-    data = request.json
-    username = data.get('username')
-    password = data.get('password')
-
-    if username:
-        user.username = username
-    if password:
-        user.set_password(password)
-
-    db.session.commit()
-    return jsonify({"message": "User modifié"}), 200
-
-
-
-
-# Ajouter sécurité
-@app.route('/api/users/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    """
-    Route pour supprimer un utilisateur
-    
-    user_id : utilisateur à supprimer 
-    """
-    
-    user = db.session.get(User, user_id)
-    if not user:
-        return jsonify({"error": "Utilisateur non existant"}), 404
-
-    db.session.delete(user)
-    db.session.commit()
-    return jsonify({"message": "User supprimé"}), 200
-
-
-######################  LOGIN  #######################
-
-@app.route('/api/login', methods=['POST'])
-def login():
-    """
-    Route pour authentifier un utilisateur
-
-    Arguments attendus
-    username : nom de l'utilisateur
-    password : mdp de l'utilisateur
-    """
-    username = request.json.get('username', None)
-    password = request.json.get('password', None)
-
-    # Vérifier si l'utilisateur existe dans la base de données
-    user = User.query.filter_by(username=username).first()
-
-    if user and check_password_hash(user.password, password):
-        # Créer un token d'accès
-        token = create_access_token(identity=username)
-        token = str(token)
-        print(token)
-        return jsonify(token=token), 200
-
-    return jsonify({"msg": "Nom d'utilisateur ou mot de passe incorrect."}), 401
 
 
 ############################## SENSORS ###############################
@@ -486,7 +346,7 @@ def generate_sensor_data(sensor : SensorData):
 
 ########################## SIMULATION GENERATION RAPIDE ############################
 
-@app.route('/api/sensors/simulate/<sensor_id>', methods=['GET'])
+@app.route('/api/sensors/simulate/<uuid:sensor_id>', methods=['GET'])
 def simulate_sensor(sensor_id):
     """
     Route pour simuler très rapidement un capteur spécifique
@@ -511,7 +371,6 @@ def simulate_sensor(sensor_id):
 
         payload = {
             "sensor_uid": sensor_str_uid,
-            # "name": "idk yet", # a fix si nécessaire
             "timestamp": timestamp.isoformat(),
             "value": value
         }
@@ -649,7 +508,7 @@ def upload_csv(sensor_id):
         return jsonify({"error": f"Erreur lors de l'import : {str(e)}"}), 500
 
 app.register_blueprint(bp)
-
+app.register_blueprint(users_bp)
 
 
 ######################### MQTT ####################
